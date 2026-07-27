d24ab3678df99bc32b84adfe44101efb94bd04f4
Henrique Herrmann <h.herrmann@estudantes.ifg.edu.br>
2026-07-27 16:16:18 -0300

    retornando um json com detalhes da requisicao

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
        "fmt"
        "net/http"
+       "encoding/json"
        )
 
 func main(){
@@ -10,14 +11,20 @@ func main(){
 }
 
 
+/*
+Inicia um servidor http
+*/
 
 func server(port string){
 
      http.HandleFunc("/", func(w http.ResponseWriter,r *http.Request){
 
-     	fmt.Fprint(w, httpDetail(r))
+     	
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintln(w, httpDetail(r))
 
 	})
+	
 
      http.ListenAndServe(port, nil)
 }
@@ -25,21 +32,22 @@ func server(port string){
 
 
 /*
-
 Retorna os detalhes da chamada HTTP em string
-
 */
 
-func httpDetail(request *http.Request) []string {
-     details := []string{
-		"- Method: "+fmt.Sprint(request.Method)+"\n",
-		"- URL: "+fmt.Sprint(request.URL)+"\n",
-		"- Headers: "+fmt.Sprint(request.Header)+"\n",
-		"- Body: "+fmt.Sprint(request.Body)+"\n",
-		"- Host: "+fmt.Sprint(request.Host)+"\n",
-		"- RemoteAddr: "+fmt.Sprint(request.RemoteAddr)+"\n",
-		"- Form: "+fmt.Sprint(request.Form)+"\n",
-		"- Context: "+fmt.Sprint(request.Context())+"\n",
+func httpDetail(request *http.Request) string {
+
+     details := map[string]any{
+     	        "method":	request.Method,
+		"url":		request.URL.String(),
+		"headers":	request.Header,
+		"body":		request.Body,
+		"host":		request.Host,
+		"remoteAddr":	request.RemoteAddr,
+		"form":		request.Form,
 		}
-     return details
+		
+      formatedDetails, _ := json.MarshalIndent(details, "", "  ") 
+
+      return string(formatedDetails)
 }
